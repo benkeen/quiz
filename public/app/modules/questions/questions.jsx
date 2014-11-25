6d203a4cda0bf808861be4145693f2a113ad3b17
Benjamin Keen <ben.keen@gmail.com>
2014-11-24 20:28:04 -0800

    sync

---
+++ public/app/modules/questions/questions.jsx
@@ -0,0 +1,53 @@
+require([], function() {
+
+  var QuestionTable = React.createClass({
+    displayName: 'QuestionTable',
+    getQuestions: function () {
+      $.ajax({
+        url: this.props.url,
+        type: "GET",
+        dataType: "json",
+        success: function (data) {
+          this.setState({data: data.rows});
+        }.bind(this),
+        error: function (xhr, status, err) {
+          console.error(this.props.url, status, err.toString());
+        }.bind(this)
+      });
+    },
+    getInitialState: function () {
+      return {data: []};
+    },
+    componentDidMount: function () {
+      this.getQuestions();
+    },
+    render: function () {
+      return (
+        <QuestionRows data={this.state.data} />
+      );
+    }
+  });
+
+  var QuestionRows = React.createClass({
+    render: function () {
+      var rows = this.props.data.map(function (row) {
+        return (
+          <div>{row.doc.question}</div>
+        );
+      });
+
+      return (
+        <div className="commentList">
+        {rows}
+        </div>
+      );
+    }
+  });
+
+
+  React.render(
+    <QuestionTable url="http://localhost:8000/questions/_all_docs?include_docs=true" />,
+    document.getElementById('list')
+  );
+
+});
