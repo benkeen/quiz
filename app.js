96bc561b10549e075f31844ddff5c06e35debb25
Benjamin Keen <ben.keen@gmail.com>
2014-11-27 22:29:18 -0800

    folder restructure; node update to handle all URLs

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
 var path = require('path');
+var app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
+// url encoding
+app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function (req, res, next) {
+
+  // normal static file request
+  if (path.extname(req.path).length > 0) {
+    next();
+  } else {
+    req.url = '/index.html';
+    next();
+  }
+});
+
+// static file serve
+app.use(express.static(__dirname));
 app.listen(1234);
-console.log('Listening on port 1234');
